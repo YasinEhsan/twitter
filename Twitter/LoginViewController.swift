8dd77ff6fff11c831d95cc92d87691ff02766e75
Yasinehsan <yasinehsan11@gmail.com>
2019-03-28 12:34:29 -0400

    auto log in save

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -21,6 +21,8 @@ class LoginViewController: UIViewController {
         let myUrl = "https://api.twitter.com/oauth/request_token"
         
         TwitterAPICaller.client?.login(url: myUrl, success: {
+            
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
             self.performSegue(withIdentifier: "loginToHome", sender: self)
         }, failure: { (Error) in
             print("could not login")
